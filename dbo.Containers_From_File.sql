1bfe147e5959129c7d7e6e6bf36bfaeb4e2820c4
Si02 <Si02@SI02-PC>
2022-08-02 00:15:28 +1000

    v.0.3.3

--- dbo.Containers_From_File.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Containers_From_File] (
-    [Id]     INT           IDENTITY (1, 1) NOT NULL,
-    [Number] NVARCHAR (11) NOT NULL,
-    [Seal]   NVARCHAR (10) NULL,
-    [Weight] INT           NULL,
-    PRIMARY KEY CLUSTERED ([Number])
-);
-
